b99adae36589a836e2636f6ad2ebad62198a394a
MarcioZamperetti <marcioff6@hotmail.com>
2019-07-03 20:09:32 -0300

    commit via Mono Develop Version Control

--- cmd-mono-develop/cmd-mono-develop/Program.cs
+++ cmd-mono-develop/cmd-mono-develop/Program.cs
@@ -6,7 +6,7 @@ namespace cmdmonodevelop
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World22!");
         }
     }
 }
